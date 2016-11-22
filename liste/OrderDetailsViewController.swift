d591f7b4874f1a1cff3e272389f4084bf6f8d821
Daniel Riewe <daniel.riewe@solutio.de>
2016-11-22 18:48:20 +0100

    Wo soll es hingehen

---
+++ liste/OrderDetailsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  OrderDetailsViewController.swift
+//  liste
+//
+//  Created by Daniel Riewe on 22.11.16.
+//  Copyright © 2016 ddd. All rights reserved.
+//
+
+import UIKit
+
+class OrderDetailsViewController: UIViewController {
+    var tisch: Tisch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension OrderDetailsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tisch.gäste.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "orderDetailIdentifier", for: indexPath)
+        let name = tisch.gäste[indexPath.row].name
+        cell.textLabel?.text = name
+        return cell
+    }
+}
+
--- liste/OrdersTableViewController.swift
+++ liste/OrdersTableViewController.swift
@@ -92,14 +92,19 @@ class OrdersTableViewController: UITableViewController {
      }
      */
     
-    /*
+    
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
+        let destination = segue.destination as! OrderDetailsViewController
+        if segue.identifier == "orderDetailSegue" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                
+            }
+            destination.tisch = tisch
+        }
      }
-     */
+    
     
 }
